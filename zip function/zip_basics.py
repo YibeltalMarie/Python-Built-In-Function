1682e9ee715a33ce693675604acf119df49e8ee1
yibeltal marie <yibeltalmarie1228@gmail.com>
2024-12-14 14:39:13 +0300

    basics of zip function

--- zip function/zip_basics.py
+++ zip function/zip_basics.py
@@ -14,15 +14,15 @@
 list1=[1, 2, 3, 4]
 list2 = ['a', 'b', 'c', 'd']
 zipped = zip(list1,list2)
-print(type(zipped))
-print(list(zipped))
+print(type(zipped))   #output = <'class' zip>
+print(list(zipped))  #output=[(1,'a'),(2,'b'),(3,'c'),(4,'d')]
 
 # zip() with different length iterable inputs
 
 list1 = [1, 2, 3]
 list2 = ['y','i']
 zipped = zip(list1,list2) # creation of tuple continue till 'i'
-print(list(zipped))
+print(list(zipped))    #output=[(1,'y'),(2,'i')]
 
 
 # zip() with multiple iterables
@@ -33,7 +33,7 @@ grade = ['A','A+','B','A-','A']
 
 combiled_zip = zip(name, sex, grade)
 
-print(list(combiled_zip))
+print(list(combiled_zip))  #output=[('yibeltal','M','A'),('temesgen',M','A+')..]
 
 
 # unzipping the zip tuple using zip itself
@@ -41,9 +41,9 @@ print(list(combiled_zip))
 zipped_file = [(1,'a','social'),(2,'b','cs'),(29,'y','soft'),(83,'t','all')]
 unzipped_file = zip(*zipped_file)
 list1,list2,list3 = list(unzipped_file)
-print(list1)
-print(list2)
-print(list3)
+print(list1)  #output = (1,2,29,83)
+print(list2)  #output = ('a','b','y','t')
+print(list3)  #output = ('social','cs','soft','all')
 
 
 zipped = [(1,'yibeltal'),(2,"abaynew"),(3,"yared")]
@@ -52,8 +52,8 @@ unzipped = zip(*zipped)
 
 number, name = list(unzipped)
 
-print(number)
-print(name)
+print(number) #output = (1, 2, 3)
+print(name)   #output = ('yibeltal', 'abaynew', 'yared')
 
 # application of this zip function
 
@@ -62,5 +62,5 @@ information = ['yibeltal',22,'software']
 
 zipped = zip(name,information)
 
-dictionary = dict(zipped)
-print(dictionary)
+dictionary = dict(zipped)  # or dict(zip(name, information)
+print(dictionary) #{'name':'yibeltal','age':22,'department':'software'}
